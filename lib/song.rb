abec66ee7472b08bb3f1c7c86641a130182477db
Micah Bowie <micahbowie20@gmail.com>
2020-07-12 21:30:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-  
-  def initalize(name)
-    @name = name 
-  end 
- 
+
   def self.all
     @@all
   end
@@ -14,27 +10,56 @@ class Song
     self.class.all << self
   end
 
- def self.create
-   name = self.new
-   name
-    @@all << name
-    return name 
+  def self.create
+    song = self.new
+    #@@all << song
+    song
   end
   
   def self.new_by_name(name)
-    new_song = Song.new
+    song = self.new 
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name
+    @@all << song
+    song
   end
   
   def self.find_by_name(name)
-    self.all.find{|person| person.name == name}
-  end 
+    @@all.find{|x| x.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+      self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical()
+    @@all.sort_by{|x| x.name}
+  end
   
   
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
   
-  def self.destroy_all 
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
+  
+  def self.destroy_all
     @@all.clear
-  end 
-    
+  end
 
-    
+end
 end
